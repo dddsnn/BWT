8bfeda48c70076d51b853afb41dcc2ac34e8d4a3
dddsnn <marc.lehmann@gmx.de>
2014-05-30 02:46:36 +0200

    can now export files to the lkh heuristic solver

--- BWT/bwt/main.py
+++ BWT/bwt/main.py
@@ -11,7 +11,6 @@ from openopt import TSP, oosolver
 import networkx as nx
 import numpy as np
 import pickle
-from bwt.coder import bw_encode
 
 def make_transitions(in_path, out_path=None):
     '''Create the transition analysis for a file.'''
@@ -23,8 +22,9 @@ def make_transitions(in_path, out_path=None):
             pickle.dump(trs, out_file)
     return trs
 
+
 def make_graph(transitions, out_path=None):
-    # TODO
+    # TODO switch to bytes
     trs = pickle.load(open('/home/dddsnn/tmp/book1/transitions', 'rb'))
     g = nx.DiGraph()
     edges = []
@@ -46,6 +46,71 @@ def make_graph(transitions, out_path=None):
     print('graph created')
     pickle.dump(g, open('/home/dddsnn/tmp/book1/graph', 'wb'))
     return g
+
+def write_tsplib_files(graph, out_dir_path, file_name):
+    '''Create and write the .tsp and .par files for the LKH program as well as
+    the file mapping the LKH node ids back to the node names.'''
+    num_nodes = len(graph.nodes())
+    # name mapping part
+    # make dicts nodename->number and number->nodename for the tsp file
+    names_to_numbers = numbers_to_names = {}
+    for i, n in enumerate(graph.nodes(), 1):
+        names_to_numbers[n] = i
+        numbers_to_names[i] = n
+
+    # tsp file part
+    # start with the header
+    tsp_text = 'NAME: bwt\n'
+    tsp_text += 'TYPE: ATSP\n'
+    tsp_text += 'DIMENSION: {0}\n'.format(num_nodes)
+    tsp_text += 'EDGE_WEIGHT_TYPE: EXPLICIT\n'
+    tsp_text += 'EDGE_WEIGHT_FORMAT: FULL_MATRIX\n'
+    tsp_text += 'EDGE_WEIGHT_SECTION\n'
+
+    MAX_INT = 2 * 10 ** 7  # the maximum value to write to the tsp file
+    INFINITY = 2147483648  # value to signify infinity in the tsp file
+    # need to scale up all floats to integers
+    # dictionary edge->cost
+    costs = nx.get_edge_attributes(graph, 'cost')
+    # min and max edge costs, and the ratio
+    min_cost = min([abs(c) for c in costs.values() if c != 0])
+    max_cost = max([abs(c) for c in costs.values()])
+    ratio = max_cost / min_cost
+    print('ratio between smallest and largest value: {0}'.format(ratio))
+    # choose the scaling factor so that the max cost is close to MAX_INT
+    factor = int((1 / max_cost) * MAX_INT)
+    print('scaling with factor {0}'.format(factor))
+
+    max_len = len(str(INFINITY))  # length of the longest number
+    # append the scaled cost value matrix to the tsp text
+    for irow in range(1, num_nodes + 1):
+        for icol in range(1, num_nodes + 1):
+            if irow == icol:
+                # node to itself -> infinity
+                write_num = INFINITY
+            else:
+                edge = (numbers_to_names[irow], numbers_to_names[icol])
+                write_num = int(costs[edge] * factor)
+            # leave 2 characters spacing
+            space = ' ' * (max_len - len(str(write_num)) + 2)
+            # append the string
+            tsp_text += space + str(write_num)
+        # newline at the end of a row
+        tsp_text += '\n'
+    # EOF at the end of the file
+    tsp_text += 'EOF'
+
+    # par file part
+    par_text = 'PROBLEM_FILE = {0}\n'.format(file_name + '.atsp')
+    par_text += 'TOUR_FILE = ' + file_name + '.tour'
+
+    # write the files
+    with open(out_dir_path + file_name + '.atsp', 'wt') as tsp_file:
+        tsp_file.write(tsp_text)
+    with open(out_dir_path + file_name + '.par', 'wt') as par_file:
+        par_file.write(par_text)
+    with open(out_dir_path + file_name + '.nodenames', 'wb') as names_file:
+        pickle.dump(numbers_to_names, names_file)
 
 def solve_tsp():
     # TODO
@@ -74,5 +139,9 @@ def simulate_compression(in_path, order=None):
     print(res_text)
 
 if __name__ == '__main__':
-    order = b'aeioubcdgfhrlsmnpqjktwvxyzAEIOUBCDGFHRLSMNPQJKTWVXYZ'
-    simulate_compression('/home/dddsnn/Downloads/calgary/book1')
+    g = pickle.load(open('/home/dddsnn/tmp/book1/graph', 'rb'))
+    e = nx.get_edge_attributes(g, 'cost')
+    print(g.nodes())
+    write_tsplib_files(g, '/home/dddsnn/tmp/', 'tsp_text')
+#     order = b'aeioubcdgfhrlsmnpqjktwvxyzAEIOUBCDGFHRLSMNPQJKTWVXYZ'
+#     simulate_compression('/home/dddsnn/Downloads/calgary/pic')
